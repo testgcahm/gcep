e7fd1a721a067b65f5bb80e0cd5f32f0f34920e4
testgcahm <108066866+testgcahm@users.noreply.github.com>
2025-05-02 10:52:02 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+8import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -88,8 +88,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Analytics />
-        <Header />
-        {children}
+        {/* <Header />
+        {children}*\} 
       </body>
     </html>
   );
